3b7a9d7bd48c109dd9bc38fd3e485093cc44e1df
bagomh70-web <bagomh70@gmail.com>
2025-11-27 23:50:04 +0600

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
 // db.php — railway MySQL config
 
-$DB_HOST = "mysql.railway.internal";
+$DB_HOST = "ballast.proxy.rlwy.net";
 $DB_USER = "root";
 $DB_PASS = "UALGGaxYcSxFIbVTCeuNSBAOApciljsN";
 $DB_NAME = "railway";
-$DB_PORT = 3306;
+$DB_PORT = 42159;
 
 // Validate
 if (!$DB_HOST || !$DB_USER || !$DB_NAME || !$DB_PORT) {
